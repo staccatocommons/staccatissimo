842cf621a4e52aa94658d36a27441e38d902e5d1
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2010-08-02 05:35:39 +0000

    Adding test cases for MapBuilder, Function, PatternPredicate, CallableProvider, ContainsSubstring

---
+++ commons-lang/src/test/java/net/sf/staccato/commons/lang/predicate/PatternPredicateUnitTest.java
@@ -0,0 +1,39 @@
+/*
+ Copyright (c) 2010, The Staccato-Commons Team
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; version 3 of the License.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ */
+package net.sf.staccato.commons.lang.predicate;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * @author flbulgarelli
+ * 
+ */
+public class PatternPredicateUnitTest {
+
+	/**
+	 * Test method for
+	 * {@link net.sf.staccato.commons.lang.predicate.PatternPredicate#eval(java.lang.String)}
+	 * .
+	 */
+	@Test
+	public void testEvalString() {
+		assertTrue(new PatternPredicate(".*foo.*")
+			.apply("The word foo is just a placeholder"));
+		assertFalse(new PatternPredicate(".*bar.*")
+			.apply("The word foo is just a placeholder"));
+	}
+
+}
