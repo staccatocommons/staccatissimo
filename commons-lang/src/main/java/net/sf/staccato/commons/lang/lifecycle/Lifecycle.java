ebf3f6e407093694e04fc3de75fa887e5c7b8bcb
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2010-09-13 03:05:42 +0000

    Updating Lifecycle javadoc

--- commons-lang/src/main/java/net/sf/staccato/commons/lang/lifecycle/Lifecycle.java
+++ commons-lang/src/main/java/net/sf/staccato/commons/lang/lifecycle/Lifecycle.java
@@ -12,12 +12,46 @@ GNU Lesser General Public License for more details.
  */
 package net.sf.staccato.commons.lang.lifecycle;
 
-public interface Lifecycle<TargetType, ExceptionType extends Exception, ReturnType> {
+import net.sf.staccato.commons.lang.check.annotation.NonNull;
 
-	TargetType initialize() throws ExceptionType;
+/**
+ * A {@link Lifecycle} represents a flow of initialization, usage and dispose of
+ * a resource.
+ * 
+ * @author flbulgarelli
+ * 
+ * @param <ResourceType>
+ * @param <ExceptionType>
+ * @param <ResultType>
+ */
+public interface Lifecycle<ResourceType, ExceptionType extends Exception, ResultType> {
 
-	ReturnType doWork(TargetType target) throws ExceptionType;
+	/**
+	 * Initializes and gets a resource of ResourceType
+	 * 
+	 * @return the initialized resources
+	 * @throws ExceptionType
+	 */
+	@NonNull
+	ResourceType initialize() throws ExceptionType;
 
-	void dispose(TargetType target) throws ExceptionType;
+	/**
+	 * Makes usage of a resource, and returns a result
+	 * 
+	 * @param resource
+	 *          the resource to use
+	 * @return the result of using the resource, of ResultType. It may be null, if
+	 *         and only if ResultType is {@link Void}
+	 * @throws ExceptionType
+	 */
+	ResultType doWork(@NonNull ResourceType resource) throws ExceptionType;
+
+	/**
+	 * Disposes the resource.
+	 * 
+	 * @param resource
+	 * @throws ExceptionType
+	 */
+	void dispose(@NonNull ResourceType resource) throws ExceptionType;
 
 }
