a8ec5396333c08045fa10bbfc967b5fe6f66eab0
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2010-11-26 03:22:18 +0000

    Added IOStreams

---
+++ commons-io/src/main/java/net/sf/staccato/commons/io/IOStreams.java
@@ -0,0 +1,30 @@
+/*
+ Copyright (c) 2010, The Staccato-Commons Team
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; version 3 of the License.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ */
+package net.sf.staccato.commons.io;
+
+import net.sf.staccato.commons.check.annotation.NonNull;
+import net.sf.staccato.commons.collections.stream.Stream;
+import net.sf.staccato.commons.collections.stream.Streams;
+
+/**
+ * @author flbulgarelli
+ * 
+ */
+public class IOStreams {
+
+	@NonNull
+	public static <A> Stream<A> from(@NonNull Readable redable, ReadStrategy readStrategy) {
+		return Streams.from(IOIterators.from(redable, readStrategy));
+	}
+
+}
---
+++ commons-io/src/main/java/net/sf/staccato/commons/io/ReadableIterator.java
@@ -0,0 +1,41 @@
+/*
+ Copyright (c) 2010, The Staccato-Commons Team
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; version 3 of the License.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ */
+package net.sf.staccato.commons.io;
+
+import java.util.Scanner;
+
+import net.sf.staccato.commons.collections.iterable.internal.AbstractUnmodifiableIterator;
+
+/**
+ * @author flbulgarelli
+ * 
+ */
+public class ReadableIterator<A> extends AbstractUnmodifiableIterator<A> {
+
+	private final ReadStrategy<A> readStrategy;
+	private final Scanner scanner;
+
+	public ReadableIterator(ReadStrategy<A> readStrategy, Readable readable) {
+		this.readStrategy = readStrategy;
+		this.scanner = new Scanner(readable);
+	}
+
+	public boolean hasNext() {
+		return readStrategy.hasNext(scanner);
+	}
+
+	public A next() {
+		return readStrategy.next(scanner);
+	}
+
+}
