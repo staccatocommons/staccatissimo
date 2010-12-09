6f1e5f930fe9ac7f6554779ca72271fe2fe91e3d
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2010-12-09 01:45:21 +0000

    Adding SerializationAssert

--- commons-testing/src/main/java/net/sf/staccato/commons/testing/junit/Assert.java
+++ commons-testing/src/main/java/net/sf/staccato/commons/testing/junit/Assert.java
@@ -19,35 +19,25 @@ public class Assert extends org.junit.Assert {
 
 	public static <T> void assertContains(Collection<T> collection, T element) {
 		assertTrue(//
-			String.format(
-				"collection %s should contain element %s",
-				collection,
-				element),
+			String.format("collection %s should contain element %s", collection, element),
 			collection.contains(element));
 	}
 
-	public static <T> void assertContains(String message,
-		Collection<T> collection, T element) {
+	public static <T> void assertContains(String message, Collection<T> collection, T element) {
 		assertTrue(message, collection.contains(element));
 	}
 
-	public static <K, V> void assertContains(String message, Map<K, V> map,
-		K key, V value) {
+	public static <K, V> void assertContains(String message, Map<K, V> map, K key, V value) {
 		assertEquals(message, value, map.get(key));
 	}
 
-	public static <K, V> void assertContainsEntry(String message, Map<K, V> map,
-		K key, V value) {
+	public static <K, V> void assertContainsEntry(String message, Map<K, V> map, K key, V value) {
 		assertEquals(message, value, map.get(key));
 	}
 
 	public static <K, V> void assertContainsEntry(Map<K, V> map, K key, V value) {
 		assertEquals( //
-			String.format(
-				"map %s should contain key %s and value %s",
-				map,
-				key,
-				value),
+			String.format("map %s should contain key %s and value %s", map, key, value),
 			value,
 			map.get(key));
 	}
---
+++ commons-testing/src/main/java/net/sf/staccato/commons/testing/junit/SerializationAssert.java
@@ -0,0 +1,40 @@
+/*
+ Copyright (c) 2010, The Staccato-Commons Team
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; version 3 of the License.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ */
+package net.sf.staccato.commons.testing.junit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * @author flbulgarelli
+ * 
+ */
+public class SerializationAssert {
+
+	public static <T extends Serializable> void assertCanSerialize(T object) {
+		ByteArrayOutputStream ba = new ByteArrayOutputStream();
+		try {
+			new ObjectOutputStream(ba).writeObject(object);
+			T result = (T) new ObjectInputStream(new ByteArrayInputStream(ba.toByteArray())).readObject();
+			assertEquals(result, object);
+		} catch (Exception e) {
+			fail("Should not have thrown an exception " + e.toString());
+		}
+	}
+}
