b4b6761ed9efa0f649ea5f1e70b047b001a8b7e3
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2011-01-07 01:38:32 +0000

    Adding package-info

---
+++ commons-defs/src/main/java/net/sf/staccatocommons/defs/restriction/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains the source-level {@link net.sf.staccatocommons.defs.restriction.Restriction}
+ * meta-annotation, which defines semantics and behavior of annotations that express constraints, 
+ * and some concrete restriction annotations about serialization, unmodifiability, immutability
+ * and value objects.  
+ */
+package net.sf.staccatocommons.defs.restriction;
+
