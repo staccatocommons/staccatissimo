2a46de7bc8011cc0d3d51ac67accd8073518493e
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2011-02-08 17:38:24 +0000

    Deleting commons-applicables

--- commons-applicables/src/main/java/net/sf/staccatocommons/applicables/function/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains the {@link net.sf.staccatocommons.lang.function.AbstractFunction} abstract class and variants,
- *  and the {@link net.sf.staccatocommons.applicables.function.Functions} factory class methods hub, which provide
- *  rich-interfaced {@link net.sf.staccatocommons.defs.Applicable}s and variants 
- */
-package net.sf.staccatocommons.applicables.function;
-
