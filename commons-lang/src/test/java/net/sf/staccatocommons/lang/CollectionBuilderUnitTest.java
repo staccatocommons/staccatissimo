00855fc223bca59e356b44855047867bd0e3f633
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2011-09-22 20:07:01 -0300

    Adding tests for collection builder

---
+++ commons-lang/src/test/java/net/sf/staccatocommons/lang/CollectionBuilderUnitTest.java
@@ -0,0 +1,57 @@
+package net.sf.staccatocommons.lang;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.junit.Test;
+
+/**
+ * @author flbulgarelli
+ */
+public class CollectionBuilderUnitTest {
+
+  /**
+   * Tests that by default collection builder creates unmodifiable collections
+   */
+  @Test(expected = UnsupportedOperationException.class)
+  public void testSortedSetWithIsUnmodifieable() {
+    CollectionBuilder.sortedSetWith(90).with(9).with(2).build().add(1);
+  }
+
+  /**
+   * Tests that {@link CollectionBuilder#setWith(Object)} add an element to the
+   * set
+   */
+  @Test
+  public void testSetWithAddsSingleElement() {
+    assertEquals(CollectionBuilder.setWith(10).build(), new HashSet<Integer>(Arrays.asList(10)));
+  }
+
+  /**
+   * Tests that {@link CollectionBuilder#listWith(Object)} add an element to the
+   * list
+   */
+  @Test
+  public void testListWithAddsASingleElement() {
+    assertEquals(CollectionBuilder.listWith(10).build(), Arrays.asList(10));
+  }
+
+  /**
+   * Tests that
+   * {@link CollectionBuilder#withIf(Object, net.sf.staccatocommons.defs.Evaluable)}
+   * add an element to the collection only if meets the criteria
+   */
+  @Test
+  public void testWithIfAddsElementWhenMeetsCondition() {
+    assertEquals(
+      CollectionBuilder
+        .listWith(10)
+        .withIf(90, Compare.greaterThan(20))
+        .withIf(5, Compare.in_(10, 2))
+        .build(),
+      Arrays.asList(10, 90));
+  }
+
+}
