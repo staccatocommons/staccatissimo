bf888b6cbd6a6530c0f676cda582cf1edc904c65
Franco Bulgarelli <flbulgarelli@yahoo.com.ar>
2011-12-08 18:41:09 -0300

    Adding package info and index to reductions and numbers

---
+++ commons-reductions/src/main/java/net/sf/staccatocommons/reductions/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This packages contains
+ * an abstract implementation of a {@link net.sf.staccatocommons.defs.reduction.Reduction}
+ * as well as several concrete reductions, most of them modeled after 
+ * SQL-aggregate functions
+ */
+package net.sf.staccatocommons.reductions;
+
