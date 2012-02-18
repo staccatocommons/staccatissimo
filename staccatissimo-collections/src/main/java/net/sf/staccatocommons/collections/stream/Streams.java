68c574eabeabc13b5fa5e63f83acd1ecb33595f5
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2012-02-18 19:12:46 -0300

    Adding ArrayStream

--- staccatissimo-collections/src/main/java/net/sf/staccatocommons/collections/stream/Streams.java
+++ staccatissimo-collections/src/main/java/net/sf/staccatocommons/collections/stream/Streams.java
@@ -13,7 +13,6 @@
 
 package net.sf.staccatocommons.collections.stream;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Deque;
 import java.util.Enumeration;
@@ -22,6 +21,7 @@ import java.util.List;
 
 import net.sf.staccatocommons.collections.restrictions.Projection;
 import net.sf.staccatocommons.collections.restrictions.Repeatable;
+import net.sf.staccatocommons.collections.stream.internal.ArrayStream;
 import net.sf.staccatocommons.collections.stream.internal.CharSequenceStream;
 import net.sf.staccatocommons.collections.stream.internal.CollectionStream;
 import net.sf.staccatocommons.collections.stream.internal.DequeStream;
@@ -114,7 +114,7 @@ public class Streams {
   @Repeatable
   @Projection
   public static <A> Stream<A> cons(@NonNull A... elements) {
-    return from(Arrays.asList(elements));
+    return new ArrayStream(elements);
   }
 
   /**
---
+++ staccatissimo-collections/src/main/java/net/sf/staccatocommons/collections/stream/internal/ArrayStream.java
@@ -0,0 +1,54 @@
+/*
+ Copyright (c) 2012, The Staccato-Commons Team
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; version 3 of the License.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ */
+package net.sf.staccatocommons.collections.stream.internal;
+
+import net.sf.staccatocommons.iterators.thriter.Thriterator;
+import net.sf.staccatocommons.iterators.thriter.Thriterators;
+import net.sf.staccatocommons.restrictions.check.NonNull;
+
+/**
+ * @author flbulgarelli
+ * 
+ */
+public class ArrayStream<A> extends StrictStream<A> {
+
+  private final A[] array;
+
+  /**
+   * Creates a new {@link ArrayStream}
+   */
+  public ArrayStream(@NonNull A[] array) {
+    this.array = array;
+  }
+
+  public Thriterator<A> iterator() {
+    return Thriterators.from(array);
+  }
+
+  public int size() {
+    return array.length;
+  }
+
+  public A get(int n) {
+    return array[n];
+  }
+
+  public boolean isEmpty() {
+    return array.length == 0;
+  }
+
+  public A last() {
+    return array[array.length - 1];
+  }
+
+}
